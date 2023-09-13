0bd8fdbde7fef1ae3ab808b82b898fb14971a465
Tim Lin <timlin07172@gmail.com>
2023-09-12 21:18:06 -0400

    c1

---
+++ Utils.py
@@ -0,0 +1,13 @@
+class utils:
+    def reversed(number):
+        if isinstance(number,int):
+            return int(str(number)[::-1])
+        else:
+            print("Input type is wrong")
+        
+    def formatter(number):
+        if isinstance(number,int):
+            return bin(number), oct(number)
+        else:
+            print("Input type is wrong")
+            
