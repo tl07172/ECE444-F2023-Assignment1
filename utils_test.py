ebdf2ad9a8b8eed90f876f67ca80c36d14020989
tl07172 <84355002+tl07172@users.noreply.github.com>
2023-09-12 21:15:01 -0400

    Update utils_test.py

--- utils_test.py
+++ utils_test.py
@@ -1,5 +1,5 @@
 import unittest
-from Utils import Utils
+from Utils import utils
 
 class utils_tests(unittest.TestCase):
 
@@ -9,13 +9,13 @@ class utils_tests(unittest.TestCase):
         message1 = "Reversed function test failed"
         
         #test reversed with integer input
-        self.assertEqual(Utils.reversed(123),321, message1)
+        self.assertEqual(utils.reversed(123),321, message1)
         
         #test reversed with float input
-        self.assertEqual(Utils.reversed(1.23),321, message1)
+        self.assertEqual(utils.reversed(1.23),321, message1)
         
         #test reversed with string input
-        self.assertEqual(Utils.reversed("test"),321, message1)
+        self.assertEqual(utils.reversed("test"),321, message1)
        
 #Test case for formatter       
        
@@ -23,13 +23,13 @@ class utils_tests(unittest.TestCase):
         message2 = "Formatter function test failed"
         
         #test formatter with integer input
-        self.assertEqual(Utils.formatter(9),('0b1001','0o11'), message2)
+        self.assertEqual(utils.formatter(9),('0b1001','0o11'), message2)
         
         #test formatter with float input
-        self.assertEqual(Utils.formatter(9.9),('0b1001','0o11'), message2)
+        self.assertEqual(utils.formatter(9.9),('0b1001','0o11'), message2)
         
         #test formatter with string input
-        self.assertEqual(Utils.formatter("test"),('0b1001','0o11'), message2)
+        self.assertEqual(utils.formatter("test"),('0b1001','0o11'), message2)
 
 if __name__ == '__main__':
 	unittest.main()
